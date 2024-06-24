e5d7b96d183ed98d484de67bbf8882203323c2e3
FatherofElias <supra_3_8@hotmail.com>
2024-06-24 13:48:43 -0500

    Q2T1 update

---
+++ AssignmentQ2.py
@@ -0,0 +1,10 @@
+# Question 2
+
+
+#Task 1
+
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+if "Alice" in submitted and attended: # using in/and to see if "Alice" being in either list is true or false
+    print("Alice both attended class and submitted their assignmment")
